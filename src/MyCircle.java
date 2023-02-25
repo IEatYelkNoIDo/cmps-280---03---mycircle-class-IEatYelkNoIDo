63757e02f49af92b6e320a363be8bc3ce0fe1d07
Watson Rogers <watjrogers09@gmail.com>
2023-02-24 22:33:09 -0600

    My Circle: Attempt 1

--- src/MyCircle.java
+++ src/MyCircle.java
@@ -1,43 +1,72 @@
 public class MyCircle {
     /* Attributes/Data fields/Properties */
-    // YOUR CODE HERE
-
-    /* Constructor(s) */
+    private double x;
+    private double y;
+    private double radius;
     MyCircle () {
-        // YOUR CODE HERE
+        x = 0;
+        y = 0;
+        radius = 1;
     }
-    MyCircle (/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    MyCircle (double radius) {
+        x = 0;
+        y = 0;
+        if (radius > 0) {
+            this.radius = radius;
+        }
+        else {
+            this.radius = 1;
+        }
     }
-    MyCircle (/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    MyCircle (double x, double y) {
+        this.x = x;
+        this.y = y;
+        radius = 1;
     }
-    MyCircle (/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    MyCircle ( double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+
+        if (radius > 0) {
+            this.radius = radius;
+        }
+        else {
+            this.radius = 1;
+        }
     }
 
     /* Method(s) */
     // Getters and Setters
-    public /* add appropriate return type */ setRadius(/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    public double getX(/* add expected arguments here if needed */) {
+        return x;
     }
-    public /* add appropriate return type */ getRadius(/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    public void setX(double x) {
+        this.x = x;
     }
-    public /* add appropriate return type */ setX(/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    public double getY(/* add expected arguments here if needed */) {
+        return y;
     }
-    public /* add appropriate return type */ getX(/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    public void setY(double y) {
+        this.y = y;
     }
-    public /* add appropriate return type */ setY(/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    public double getRadius(/* add expected arguments here if needed */) {
+        return radius;
     }
-    public /* add appropriate return type */ getY(/* add expected arguments here if needed */) {
-        // YOUR CODE HERE
+    public void setRadius(double radius) {
+        if (radius > 0)
+            this.radius = radius;
     }
+
+
+
     // Other helpful methods
-    public /* add appropriate return type */ contains(double x, double y) {
-        // YOUR CODE HERE
+    public boolean contains(double x, double y) {
+        double line = Math.sqrt(Math.pow((this.x - x),2) + Math.pow((this.y - y),2));
+        if (line > radius){
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
